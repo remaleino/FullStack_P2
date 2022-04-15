de07169a59f2a04ecb519cfe70bef10137cc18bb
remaleino <raimond.leino@gmail.com>
2022-04-15 20:07:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const indexRouter = require('./routes/index');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 // view engine setup
 
 app.set('views', path.join(__dirname, 'views'));
@@ -10,5 +11,6 @@ app.use('/', indexRouter);
 app.use(express.static('public'))
 
 
-app.listen(3000)
-console.log('listening on port 3000')
+app.listen(PORT, () => {
+    console.log('listening on port 3000')
+});
